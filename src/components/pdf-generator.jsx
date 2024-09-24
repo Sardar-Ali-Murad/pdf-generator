0368ed8474995e3174f7f243751214d1391a4a4a
Sardar-Ali-Murad <mar389786@gmail.com>
2024-09-24 10:26:41 -0700

    style changes

--- src/components/pdf-generator.jsx
+++ src/components/pdf-generator.jsx
@@ -26,6 +26,14 @@ const styles = StyleSheet.create({
     paddingLeft: 35,
     paddingRight: 35,
   },
+  head: {
+    fontFamily: "Poppins",
+    color: "violet",
+  },
+  subHead: {
+    fontFamily: "Poppins",
+    color: "green",
+  },
   globalFont: {
     fontFamily: "Poppins",
   },
@@ -118,6 +126,31 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     flexDirection: "row",
   },
+  flexDataWhy: {
+    display: "flex",
+    gap: 10,
+    fontSize: 12,
+    marginBottom: 10,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  flexItems: {
+    display: "flex",
+    gap: 10,
+    fontSize: 12,
+    marginBottom: 10,
+    flexDirection: "row",
+    color: "violet",
+    flexWrap: "wrap",
+    marginLeft: 10,
+  },
+  datadescription: {
+    display: "flex",
+    gap: 10,
+    fontSize: 12,
+    marginBottom: 10,
+    flexDirection: "column",
+  },
   htmlData: {
     fontSize: 12,
   },
@@ -160,36 +193,44 @@ const PDFGenerator = ({ data }) => {
                 {ind + 1}. {item?.title}
               </Text>
               <View style={styles.flexData}>
-                <Text style={styles.globalFont}>ID: {item?._id}</Text>
+                <Text style={styles.head}>ID : </Text>
+                <Text style={styles.subHead}>{item?._id}</Text>
               </View>
               <View style={styles.flexData}>
-                <Text style={styles.globalFont}>Title: {item?.title}</Text>
+                <Text style={styles.head}>Title : </Text>
+                <Text style={styles.subHead}>{item?.title}</Text>
               </View>
               <View style={styles.flexData}>
-                <Text style={styles.globalFont}>
-                  vendorId: {item?.vendorId}
-                </Text>
+                <Text style={styles.head}>vendorId : </Text>
+                <Text style={styles.subHead}>{item?.vendorId}</Text>
               </View>
               <View style={styles.datadescription}>
-                <Text style={styles.description}> Description:</Text>
-                <Html style={styles.htmlData}>{item?.description}</Html>
+                <Text style={styles.head}>description : </Text>
+                <Html style={styles.subHead}>{item?.description}</Html>
               </View>
-              <View style={styles.flexData}>
-                <Text style={styles.globalFont}>Address: {item?.address}</Text>
-              </View>
-              <View style={styles.flexData}>
-                <Text style={styles.globalFont}>
-                  MemberShip: {item?.memberShip}
-                </Text>
-              </View>
+              {item?.address && item?.address !== "" && (
+                <View style={styles.flexData}>
+                  <Text style={styles.head}>Address : </Text>
+                  <Text style={styles.subHead}>{item?.address}</Text>
+                </View>
+              )}
+              {item?.memberShip && item?.memberShip !== "" && (
+                <View style={styles.flexData}>
+                  <Text style={styles.head}>MemberShip :</Text>
+                  <Text style={styles.subHead}>{item?.memberShip}</Text>
+                </View>
+              )}
               {item?.duration && item?.duration?.length > 0 && (
                 <View style={styles.flexData}>
-                  <Text style={styles.globalFont}>Durations:</Text>
-                  <View>
+                  <Text style={styles.head}>Durations : </Text>
+                  <View style={styles.flexItems}>
                     {item?.duration?.map((singleItem, ind) => (
-                      <Text key={ind} style={styles.globalFont}>
-                        {singleItem}
-                      </Text>
+                      <React.Fragment key={ind}>
+                        <Text style={styles.subHead}>
+                          {singleItem}
+                          {ind < item.duration.length - 1 && ","}
+                        </Text>
+                      </React.Fragment>
                     ))}
                   </View>
                 </View>
@@ -197,24 +238,29 @@ const PDFGenerator = ({ data }) => {
               {item?.storyLineKeywords &&
                 item?.storyLineKeywords?.length > 0 && (
                   <View style={styles.flexData}>
-                    <Text style={styles.globalFont}>StoryLine Keywords:</Text>
-                    <View>
+                    <Text style={styles.head}>StoryLine Keywords : </Text>
+                    <View style={styles.flexItems}>
                       {item?.storyLineKeywords?.map((singleItem, ind) => (
-                        <Text key={ind} style={styles.globalFont}>
-                          {singleItem}
-                        </Text>
+                        <React.Fragment key={ind}>
+                          <Text style={styles.subHead}>
+                            {singleItem}{" "}
+                            {ind < item.storyLineKeywords.length - 1 && ","}
+                          </Text>
+                        </React.Fragment>
                       ))}
                     </View>
                   </View>
                 )}
               {item?.price && item?.price?.length > 0 && (
                 <View style={styles.flexData}>
-                  <Text style={styles.globalFont}>Prices:</Text>
-                  <View>
+                  <Text style={styles.head}>Prices : </Text>
+                  <View style={styles.flexItems}>
                     {item?.price?.map((singleItem, ind) => (
-                      <Text key={ind} style={styles.globalFont}>
-                        {singleItem}
-                      </Text>
+                      <React.Fragment key={ind}>
+                        <Text style={styles.subHead}>
+                          {singleItem} {ind < item.price.length - 1 && ","}
+                        </Text>
+                      </React.Fragment>
                     ))}
                   </View>
                 </View>
@@ -222,23 +268,26 @@ const PDFGenerator = ({ data }) => {
               {item?.linkWithOtherExperience &&
                 item?.linkWithOtherExperience?.length > 0 && (
                   <View style={styles.flexData}>
-                    <Text style={styles.globalFont}>Other Experiences:</Text>
+                    <Text style={styles.head}>Other Experiences : </Text>
                     <View>
                       {item?.linkWithOtherExperience?.map((singleItem, ind) => (
                         <View key={ind}>
                           <View style={styles.flexData}>
-                            <Text style={styles.globalFont}>
-                              ExperienceId: {singleItem?.experienceId}
+                            <Text style={styles.head}>ExperienceId :</Text>
+                            <Text style={styles.subHead}>
+                              {singleItem?.experienceId}
                             </Text>
                           </View>
                           <View style={styles.flexData}>
-                            <Text style={styles.globalFont}>
-                              ExperienceName: {singleItem?.experienceName}
+                            <Text style={styles.head}>ExperienceName :</Text>
+                            <Text style={styles.subHead}>
+                              {singleItem?.experienceName}
                             </Text>
                           </View>
-                          <View style={styles.flexData}>
-                            <Text style={styles.globalFont}>
-                              Why: {singleItem?.why}
+                          <View style={styles.flexDataWhy}>
+                            <Text style={styles.head}>Why :</Text>
+                            <Text style={styles.subHead}>
+                              {singleItem?.why}
                             </Text>
                           </View>
                         </View>
